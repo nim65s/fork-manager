6254a9d1b3a7ec5a494d8520493aadf5efecccc1
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-22 17:22:21 +0200

    implement dry run

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ async fn main() -> anyhow::Result<()> {
     let mut args = Args::parse();
     if args.process()? {
         let config = Config::new(&args).await?;
-        if !&args.dry_run {
+        if args.dry_run {
+            dbg!(config);
+        } else {
             config.process(&args).await?;
         }
     }
