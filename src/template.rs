07abb8e0090507e3cb98e6ed68115d2ee1702a07
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-27 16:06:32 +0200

    write update script

--- src/template.rs
+++ src/template.rs
@@ -1,3 +1,8 @@
+use std::fs::{set_permissions, File};
+use std::io::Write;
+#[cfg(target_family = "unix")]
+use std::os::unix::fs::PermissionsExt;
+
 use minijinja::syntax::SyntaxConfig;
 use minijinja::{context, Environment};
 
@@ -22,15 +27,22 @@ pub fn generate(fm: &ForkManager) -> Result<()> {
     env.add_filter("remote_name", remote_name);
     env.add_template("update.sh", include_str!("update.sh"))?;
     let tmpl = env.get_template("update.sh").unwrap();
-    println!(
-        "{}",
-        tmpl.render(context! {
-            config => fm.config.config,
-            forks => fm.config.forks,
-            remotes => fm.config.remotes(),
-            push => fm.args.push,
-        })
-        .unwrap()
-    );
+    let ctx = context! {
+        config => fm.config.config,
+        forks => fm.config.forks,
+        remotes => fm.config.remotes(),
+        push => fm.args.push,
+    };
+    let script = fm.args.project.join("update.sh");
+    let mut file = File::create(&script)?;
+    let content = tmpl.render(ctx)?;
+    file.write_all(content.as_bytes())?;
+    file.write_all(&[b'\n'])?;
+    #[cfg(target_family = "unix")]
+    {
+        let mut perms = file.metadata()?.permissions();
+        perms.set_mode(0o755);
+        set_permissions(script, perms)?;
+    }
     Ok(())
 }
